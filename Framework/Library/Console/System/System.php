eb8b14d02c23198f7ffdab262e76e4365a49d22e
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-12 14:43:36 +0100

    Extend copyright to 2011.

--- Framework/Library/Console/System/System.php
+++ Framework/Library/Console/System/System.php
@@ -9,7 +9,7 @@
  * GNU General Public License
  *
  * This file is part of Hoa Open Accessibility.
- * Copyright (c) 2007, 2010 Ivan ENDERLIN. All rights reserved.
+ * Copyright (c) 2007, 2011 Ivan ENDERLIN. All rights reserved.
  *
  * HOA Open Accessibility is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ namespace Hoa\Console\System {
  * This class is a thin layer to get a system access.
  *
  * @author     Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright (c) 2007, 2010 Ivan ENDERLIN.
+ * @copyright  Copyright (c) 2007, 2011 Ivan ENDERLIN.
  * @license    http://gnu.org/licenses/gpl.txt GNU GPL
  */
 
