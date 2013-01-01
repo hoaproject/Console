dd026fdab7e858a7d7415ac5dde82c4ecfa6c965
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-01-01 13:20:34 +0100

    Happy new year \o/.

--- Processus.php
+++ Processus.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2012, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2013, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -73,7 +73,7 @@ namespace Hoa\Console {
  * Manipulate a processus as a stream.
  *
  * @author     Ivan Enderlin <ivan.enderlin@hoa-project.net>
- * @copyright  Copyright © 2007-2012 Ivan Enderlin.
+ * @copyright  Copyright © 2007-2013 Ivan Enderlin.
  * @license    New BSD License
  */
 
